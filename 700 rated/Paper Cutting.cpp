4b60c423757c4c0843d3fc91fa8ddaa851f3dfa6
nick2726 <nikhilnkj2627@gmail.com>
2025-10-08 02:56:43 +0530

    Create Paper Cutting.cpp

---
+++ 700 rated/Paper Cutting.cpp
@@ -0,0 +1,46 @@
+Problem Link:
+https://www.codechef.com/problems/CUTPAPER?tab=statement
+
+Solution :
+#pragma GCC optimize("O2,no-stack-protector,unroll-loops,fast-math")
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define PB push_back
+#define F first
+#define S second
+#define uset unordered_set
+#define umap unordered_map
+#define MP make_pair;
+#define vt vector
+#define all(x) begin(x), end(x)
+#define sz(x) (int)x.size()
+#define REP(i, a) for (int i = 0; i < a; i++)
+#define pii pair<int, int>;
+#define pll pair<ll, ll>;
+
+using namespace std;
+
+typedef vector<ll> vl;
+typedef pair<ll,ll> pl;
+
+void solve()
+{
+    ll n,k;
+    cin>>n>>k;
+    ll maximu_numbers=pow(n/k,2);
+    cout<<maximu_numbers<<endl;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+   	ll t;
+    	cin >> t;
+    	while (t--)
+    	{
+        	solve();
+    	}
+    	return 0;
+}
